e1387cda38de4fe4b346b027a36d83c6c3cc5f95
bmihaljevic <boki.mihaljevic@gmail.com>
2018-03-20 19:38:49 +0100

    add branch test

--- tests/testthat/test.r
+++ tests/testthat/test.r
@@ -7,3 +7,10 @@ test_that("Convert nominal", {
     neurostrcpp::convert(file, file2)
     expect_true(file.exists(file2))
 })
+
+test_that("Compute branch features nominal", {
+  file <- '/home/bmihaljevic/code/bbp-data/data/BBP_SWC/C030502A.swc'
+  json_branch <- neurostrcpp::compute_branch_features(file)
+  branch <- json2dataframe(json_branch)
+  expect_true('partition_asymmetry' %in% colnames(branch))
+})
