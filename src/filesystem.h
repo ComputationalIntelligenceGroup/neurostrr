62c5923af0e7e9342190a37ebb9db15a66e17b5e
bmihaljevic <bmihaljevic@fi.upm.es>
2018-03-26 12:07:58 +0200

    filesystem import string

--- src/filesystem.h
+++ src/filesystem.h
@@ -1,23 +1,24 @@
 #ifndef NEUROSTRLIB_IO_FILESYSTEM_H
 #define NEUROSTRLIB_IO_FILESYSTEM_H
-#include <algorithm> 
+#include <algorithm>
+#include <string>
 
 namespace neurostr {
 namespace io{
-  
+
   /**
    * @brief very simple replacements for boost stem and extension
-   * @param ext File path 
+   * @param ext File path
    * @return std::string extension
-   */ 
-  std::string get_extension(const std::string& path); 
+   */
+  std::string get_extension(const std::string& path);
 
 
   /**
    * @brief very simple replacements for boost stem and extension
-   * @param ext File path 
+   * @param ext File path
    * @return std::string stem
-   */ 
+   */
   std::string get_stem(const std::string& path);
 
 }
