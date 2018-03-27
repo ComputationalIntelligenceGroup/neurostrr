4b8dde8b41da90252fa0ff0d470ba1b380c70bec
bmihaljevic <bmihaljevic@fi.upm.es>
2018-03-27 10:57:59 +0200

    updateing neuroconverter

--- .diffs/NeuroConverter.cpp
+++ .diffs/NeuroConverter.cpp
@@ -10,6 +10,7 @@
 #include "parser_dispatcher.h"
 #include "SWCWriter.h"
 #include "JSONWriter.h"
+#include "neuro_converter.h"
 
 //' Converts a reconstruction to SWC or JSON.
 //'
@@ -19,55 +20,12 @@ void convert(std::string ifile,  std::string ofile, bool correct = false)
 {
 
   neurostr::log::init_log_cerr();
-  neurostr::log::enable_log();
-
-  // Output Format
-  std::string ext = neurostr::io::get_extension(ofile);
-
-  // Apply RDP simplification
-  float eps = 0.0;
-  // ("help,h", "Produce help message")
-  // ("input,i", po::value< std::string >(&ifile), "Neuron reconstruction file")
-  // ("format,f", po::value< std::string>(&ext), "Output format (swc or json)")
-  // ("output,o", po::value< std::string>(&ofile), "Output file")
-  // ("correct,c", "Try to correct errors in the reconstruction")
-  // NSTR_LOG_(error, "Input/output file required");
-
-  // Create ofstream /ifstreams
-  std::ofstream ofs(ofile);
-
-  // Read
-  auto r = neurostr::io::read_file_by_ext(ifile);
-
-  // Simpify / correct
-  for(auto it = r->begin(); it != r->end(); ++it){
-    if(correct) it->correct();
-    if(eps != 0.0 ){
-      it->simplify(eps);
-    }
-  }
+  neurostr::log::enable_log(); 
 
   // Transform extension to lower
   std::transform(ext.begin(),ext.end(),ext.begin(),::tolower);
 
-  // Select a writer depending on the extension
-  if(ext == "swc"){
-    if(r->size() > 1){
-      NSTR_LOG_(warn, "The output SWC file will only contain the first neuron in the reconstruction. The rest are ignored");
-    }
-    neurostr::io::SWCWriter writer(ofs);
-    writer.write(*(r->begin()));  // Writes first neuron
 
-  } else if (ext == "json"){
-    neurostr::io::JSONWriter writer(ofs);
-    writer.write(*r);
-  } else {
-    // Error - Unrecognized
-    NSTR_LOG_(error,"Unrecognized output format");
-    std::cout << "Accepted formats: swc, json" << std::endl << std::endl ;
-    return;
-  }
+  convert(ifile, ofile, ext, correct, eps); 
 
-  // Close stream - Reconstruction should be autom. free'd (unique_ptr)
-  ofs.close();
 }
