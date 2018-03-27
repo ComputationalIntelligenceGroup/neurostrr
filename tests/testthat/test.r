916ae3d3fa1ee6dd96a99409b45dd185d674b7b0
bmihaljevic <bmihaljevic@fi.upm.es>
2018-03-27 15:28:12 +0200

    add test neuron

--- tests/testthat/test.r
+++ tests/testthat/test.r
@@ -1,7 +1,7 @@
 context("general")
 
 test_that("Convert nominal", {
-    file <- '/home/bmihaljevic/code/bbp-data/data/BBP_SWC/C030502A.swc'
+    file <- system.file("extdata", "C030397A-P2.swc", package = "neurostr")
     file2 <- '/tmp/neurostr-test-convert.json'
     convert(file, file2, ext = "json")
     expect_true(file.exists(file2))
@@ -11,14 +11,14 @@ test_that("Convert nominal", {
 })
 
 test_that("Compute branch features nominal", {
-  file <- '/home/bmihaljevic/code/bbp-data/data/BBP_SWC/C030502A.swc'
+  file <- system.file("extdata", "C030397A-P2.swc", package = "neurostr")
   json_branch <- compute_branch_features(file)
   branch <- json2dataframe(json_branch)
   expect_true('partition_asymmetry' %in% colnames(branch))
 })
 
 test_that("Validator nominal", {
-  file <- '/home/bmihaljevic/code/bbp-data/data/BBP_SWC/C030502A.swc'
+  file <- system.file("extdata", "C030397A-P2.swc", package = "neurostr")
   validate <- validate(file)
   expect_true(nchar(validate) > 0)
 
